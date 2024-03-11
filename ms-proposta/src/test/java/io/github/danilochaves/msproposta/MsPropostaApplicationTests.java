377c0e91b2b3eaa669f4f1f14bf52be82afd4bcc
Danilo Chaves <danchavesds@gmail.com>
2024-03-11 16:49:56 -0300

    commit inicial

---
+++ ms-proposta/src/test/java/io/github/danilochaves/msproposta/MsPropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.danilochaves.msproposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPropostaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
