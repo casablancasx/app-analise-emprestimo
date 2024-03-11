0d4d80be59de165ac998f032bd94c1c081febbf5
Danilo Chaves <danchavesds@gmail.com>
2024-03-11 17:01:38 -0300

    Proposta Request e Response dtos

---
+++ ms-proposta/src/main/java/io/github/danilochaves/msproposta/dto/PropostaResponseDto.java
@@ -0,0 +1,23 @@
+package io.github.danilochaves.msproposta.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class PropostaResponseDto {
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String telefone;
+    private String cpf;
+    private Double renda;
+    private String valorSolicitadoFmt;
+    private int prazoPagamento;
+    private Boolean aprovada;
+    private String observacao;
+}
