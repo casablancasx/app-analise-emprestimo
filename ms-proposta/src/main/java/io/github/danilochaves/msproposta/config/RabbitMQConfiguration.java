bf5fceee00db4ae4d25913eeebf83eb2373127f5
Danilo Chaves <danchavesds@gmail.com>
2024-03-11 18:37:05 -0300

    RabbitMQConfiguration: Fila, Binding e Exchange para proposta pendente

---
+++ ms-proposta/src/main/java/io/github/danilochaves/msproposta/config/RabbitMQConfiguration.java
@@ -0,0 +1,53 @@
+package io.github.danilochaves.msproposta.config;
+
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguration {
+
+    @Value("${rabbitmq.queue.proposta-pendente}")
+    private String filaPropostaPendente;
+
+    @Value("${rabbitmq.exchange.proposta-pendente}")
+    private String exchangePropostaPendente;
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory){
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializarRabbitAdmin(RabbitAdmin rabbitAdmin){
+        return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Queue filaPropostaPendenteMsAnaliseCredito(){
+        return QueueBuilder.durable(filaPropostaPendente).build();
+    }
+
+    @Bean
+    public FanoutExchange propostaPendenteExchange(){
+        return ExchangeBuilder.fanoutExchange(exchangePropostaPendente).build();
+    }
+
+    @Bean
+    public Binding bindingPropostaPendenteMsAnaliseCredito(){
+        return BindingBuilder.bind(filaPropostaPendenteMsAnaliseCredito()).to(propostaPendenteExchange());
+    }
+}
